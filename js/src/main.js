cd3d408b9be048c9771196d1aebb146d3293c7d3
Robin De Kesel <robindekesel@gmail.com>
2018-03-23 11:29:38 +0100

    Blaster product detail vormgeven

--- js/src/main.js
+++ js/src/main.js
@@ -9,3 +9,18 @@ $('.btnShoppingCart').popover({
         return clone;
     }
 })
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 750);
+          return false;
+        }
+      }
+    });
+  });
