39dbbe03d541004d5312f64cf766fa02e737afe9
Robin De Kesel <robindekesel@gmail.com>
2018-05-29 13:13:47 +0200

    online

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-include("db_connOFF.php");
+include("db_conn.php");
 
 
 $query = "SELECT * FROM users";
@@ -286,7 +286,8 @@ if(isset($_POST['name'])&&isset($_POST['email'])&&isset($_POST['subject'])){
     <div id="section4">
 
         <?php
-        $connect = mysqli_connect('localhost', 'root', '', 'cart');
+        $connect =  mysqli_connect('localhost', 'dekeselr', 'jBsgC3M2', 'dekeselr');
+
         $query = 'SELECT * FROM products ORDER by id ASC';
 
         $result = mysqli_query($connect, $query);
