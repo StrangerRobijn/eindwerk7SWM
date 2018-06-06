d7a4d11847ebe42b87a550651bccdd42c2da110e
Robin De Kesel <robindekesel@gmail.com>
2018-06-06 18:15:13 +0200

    no message

--- pay.php
+++ pay.php
@@ -206,6 +206,38 @@ if(isset($_POST['number'])&&isset($_POST['email'])&&isset($_POST['adres'])){
           <script src="js/dist/jquery-2.1.4.js"></script>
           <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 
+
+<!-- DIt wou niet werken in de main.js -->
+<script>
+
+$(document).ready(function() {
+    var panelOne = $('.form-panel.two').height(),
+      panelTwo = $('.form-panel.two')[0].scrollHeight;
+  
+    $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
+      e.preventDefault();
+  
+      $('.form-toggle').addClass('visible');
+      $('.form-panel.one').addClass('hidden');
+      $('.form-panel.two').addClass('active');
+      $('.form').animate({
+        'height': panelTwo
+      }, 200);
+    });
+  
+    $('.form-toggle').on('click', function(e) {
+      e.preventDefault();
+      $(this).removeClass('visible');
+      $('.form-panel.one').removeClass('hidden');
+      $('.form-panel.two').removeClass('active');
+      $('.form').animate({
+        'height': panelOne
+      }, 200);
+    });
+  });
+
+  </script>
+
           
 </body>
 </html>
